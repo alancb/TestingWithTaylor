e4ac23f0a49e72a19f619927023b998036dde82b
Taylor Mott <trm36@humboldt.edu>
2014-09-11 11:57:54 -0600

    STEP 3: Create an addScoreView Method
    - Create an addScoreView method that will add a UIView to the scrollView
    - In the method Initialize a UIView called view
    - Initialize a UITextField for name, a UILabel for score, and a UIStepper for a button
    - Lay them out by setting their frames in the new view
    - Add that view to the scrollview
    - You'll want to adjust the properties of each of those controls:
    -- Placeholder text for the textfields
    -- Min and max values for the stepper
    
    Signed-off-by: Taylor Mott <trm36@humboldt.edu>

--- Score Keeper/SKViewController.m
+++ Score Keeper/SKViewController.m
@@ -8,9 +8,16 @@
 
 #import "SKViewController.h"
 
+static CGFloat margin = 15;                             //side margins
+static CGFloat scoreViewHeight = 64;                    //scoreViewHeight
+static CGFloat percentWidthOfNameField = 0.55;
+static CGFloat percentWidthOfScoreLabel = 0.15;
+static CGFloat percentWidthOfStepper = 0.30;
+
 @interface SKViewController ()
 
 @property (strong, nonatomic) UILabel *label;
+@property (strong, nonatomic) UIScrollView * scrollView;
 
 @end
 
@@ -19,15 +26,45 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
     self.title = @"TRM ScoreKeeper";
-    UIScrollView * scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
-    scrollView.contentSize = CGSizeMake(self.view.frame.size.width, self.view.frame.size.height * 1.5);
-    [self.view addSubview:scrollView];
+    
+    self.scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
+    self.scrollView.contentSize = CGSizeMake(self.view.frame.size.width, self.view.frame.size.height * 1.5);
+    [self.view addSubview:self.scrollView];
+    
+    //Add two scoreviews for testing
+    [self addScoreView:0];
+    [self addScoreView:1];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)addScoreView:(int)index
+{
+    UIView * scoreView = [[UIView alloc] initWithFrame: CGRectMake(0, scoreViewHeight * index, self.view.frame.size.width, scoreViewHeight)];
+    
+    CGFloat widthOfNameField = (self.view.frame.size.width - 2 * margin) * percentWidthOfNameField;
+    CGFloat widthOfScoreLabel = (self.view.frame.size.width - 2 * margin) * percentWidthOfScoreLabel;
+    CGFloat widthOfStepper = (self.view.frame.size.width - 2 * margin) * percentWidthOfStepper;
+    
+    UITextField * nameField = [[UITextField alloc] initWithFrame:CGRectMake(margin, 0, widthOfNameField, scoreViewHeight)];
+    nameField.placeholder = @"Name";
+    
+    UILabel * scoreLabel = [[UILabel alloc] initWithFrame:CGRectMake(margin + widthOfNameField, 0, widthOfScoreLabel, scoreViewHeight)];
+    scoreLabel.text = @"0";
+    
+    UIStepper * stepper = [[UIStepper alloc] initWithFrame:CGRectMake(margin + widthOfNameField + widthOfScoreLabel, 16, widthOfStepper, scoreViewHeight)];
+    stepper.minimumValue = -1000;
+    stepper.maximumValue = 9999;
+    
+    [scoreView addSubview:nameField];
+    [scoreView addSubview:scoreLabel];
+    [scoreView addSubview:stepper];
+    [self.scrollView addSubview:scoreView];
 }
 
 /*
